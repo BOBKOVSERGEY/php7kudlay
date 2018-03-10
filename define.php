96ac75b8eda250a6e55fa4194115dba946f63b47
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-03-10 20:35:59 +0300

    Задание констант массивов/Константа БД

---
+++ define.php
@@ -0,0 +1,17 @@
+<?php
+define('ANIMALS', [
+  'dog',
+  'cat',
+  'bird'
+]);
+
+echo ANIMALS[1];
+
+define('DB', [
+  'host' => 'localhost',
+  'user' => 'user',
+  'pass' => 'password',
+  'dbname' => 'dbname'
+]);
+
+echo DB['host'];
